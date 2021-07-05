96556ebf9154091a41d458a1ac480b59f064d0c2
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-07-05 22:34:35 +0900

    Add files via upload

---
+++ Variable/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age;
+        age = 21;
+
+        int num = 19;
+        age = 23 ;
+
+        age = num;
+        age = age+ 1;
+
+        System.out.print("Hello, I'm ");
+        System.out.print(age);
+        System.out.print(".");
+    }
+}
