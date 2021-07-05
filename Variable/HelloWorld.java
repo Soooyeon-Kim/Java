a9be9cd0062d8ff3aaf35ecff0090562abea5e67
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-07-05 22:34:12 +0900

    Add files via upload

---
+++ Variable/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Sooyeon on 07/05/21
+ * comment
+ * comment
+ */
+// one-line comment
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
