9aa07ac8edde8795bced0262dbca5e1d778c2edf
Soooyeon-Kim <83583349+Soooyeon-Kim@users.noreply.github.com>
2021-07-12 12:08:31 +0900

    Add files via upload

---
+++ Variable/introduction.java
@@ -0,0 +1,15 @@
+# 자기소개서 만들기
+# 변수 & 연산
+public class Main {
+    public static void main(String[] args) {
+        String name = "sooyeon";
+     
+        int month = 4;
+        int day = 12;
+        boolean graduation = false;
+        
+        System.out.println("이름: " + name);
+        System.out.println("생일: " + month + "월 " + day + "일");
+        System.out.println("졸업: " + graduation);
+    } 
+}
